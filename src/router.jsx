94f74c0559a9850284cb9676231ad0bdd1674239
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-08-18 11:17:04 -0300

    feat(front): estilização fale conosco

--- src/router.jsx
+++ src/router.jsx
@@ -1,14 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import RootLayout from "./pages/RootLayout";
+import FaleConosco from "./pages/Fale-Conosco";
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <RootLayout />,
+        element: <RootLayout/>,
         children: [{
             index: true,
             element: <Home/>
+        },
+        {
+            path: '/faleconosco',
+            element: <FaleConosco/>
         }]
     }
 ])
