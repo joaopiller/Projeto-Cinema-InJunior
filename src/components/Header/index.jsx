4207d732cd358a971107537c215bf7b65296c268
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-08-18 16:42:00 -0300

    feat(front): criei arquivos da pag filmes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className={styles.header}>
       <Link to='/'><img className={styles.headerLogo} src='/src/assets/Logo.svg' alt="Logo Cinema" /></Link>
       <nav className={styles.linkIcons}>
-        <Link to=""><img src="/src/assets/Icone-Filmes.svg" alt="Filmes"/></Link>
+        <Link to="/filmes"><img src="/src/assets/Icone-Filmes.svg" alt="Filmes"/></Link>
         <Link to=""><img src="/src/assets/Icone-Entrar.svg" alt="Entrar"/></Link>
         <Link to="/faleconosco"><img src="/src/assets/Icone-Ajuda.svg" alt="Ajuda" /></Link>
       </nav>
