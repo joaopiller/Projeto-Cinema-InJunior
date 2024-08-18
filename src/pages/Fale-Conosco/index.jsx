e94620a53bd00c554d81c02292b9e8768e9e44e4
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-08-18 11:36:41 -0300

    feat(front); add comportamento nos inputs fale conosco

--- src/pages/Fale-Conosco/index.jsx
+++ src/pages/Fale-Conosco/index.jsx
@@ -1,17 +1,52 @@
+import { useState } from 'react'
 import styles from './styles.module.css'
 
 export default function FaleConosco() {
+    const [name, setName] = useState('')
+    const [content, setContent] = useState('')
+    const [description, setDescription] = useState('')
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        setName('')
+        setContent('')
+        setDescription('')
+    }
+
     return (
         <div className={styles.contactContainer}>
             <div className={styles.formHeader}>
                 <h1>Contato</h1>
                 <h2>Encontrou algum problema? <br/> Envie uma mensagem!</h2>
             </div>
-            <form className={styles.formInputs}>
-                <input type="text" placeholder='Nome Completo' />
-                <input type="text" placeholder='Assunto'/>
-                <textarea name="description" placeholder='Descrição Detalhada'></textarea>
-                <button className={styles.submitButton} type='submit'>Enviar</button>
+            <form onSubmit={handleSubmit} className={styles.formInputs}>
+                <input 
+                    type="text" 
+                    placeholder='Nome Completo' 
+                    value={name} 
+                    onChange={(e) => setName(e.target.value)}
+                    required 
+                />
+                <input 
+                    type="text" 
+                    placeholder='Assunto' 
+                    value={content} 
+                    onChange={(e) => setContent(e.target.value)}
+                    required
+                />
+                <textarea 
+                    name="description" 
+                    placeholder='Descrição Detalhada' 
+                    value={description} 
+                    onChange={(e) => setDescription(e.target.value)}
+                    required>
+                </textarea>
+                <button 
+                    className={styles.submitButton} 
+                    type='submit'
+                >
+                    Enviar
+                </button>
             </form>
         </div>
     )
