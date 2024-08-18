d761bd682a71d5612b715fa54bb2c0c5c119070c
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-08-18 13:51:19 -0300

    feat(front): criei o modal fale conosco

--- src/pages/Fale-Conosco/index.jsx
+++ src/pages/Fale-Conosco/index.jsx
@@ -5,12 +5,24 @@ export default function FaleConosco() {
     const [name, setName] = useState('')
     const [content, setContent] = useState('')
     const [description, setDescription] = useState('')
+    const [modal, setModal] = useState(false)
 
     function handleSubmit(e) {
         e.preventDefault()
         setName('')
         setContent('')
         setDescription('')
+        toggleModal()
+    }
+
+    function toggleModal() {
+        setModal(!modal)
+    } 
+
+    if (modal) {
+        document.body.style.overflowY = 'hidden'
+    } else {
+        document.body.style.overflowY = 'auto'
     }
 
     return (
@@ -48,6 +60,17 @@ export default function FaleConosco() {
                     Enviar
                 </button>
             </form>
+            {modal && (
+                <div className={styles.modal}>
+                <div className={styles.overlay} onClick={toggleModal}></div>
+                <div className={styles.modalContent}>
+                    <h2>Mensagem Enviada!</h2>
+                    <h3>Obrigado por compartilhar suas observações conosco.</h3>
+                    <p>Sua contribuição é fundamental para melhorarmos continuamente a sua experiência em nosso site. Valorizamos seu tempo e dedicação ao relatar esse problema.</p>
+                    <button className={styles.closeModal} onClick={toggleModal}><img src="/src/assets/CloseModal.svg" alt="Fechar"/></button>
+                </div>
+            </div>
+            )}
         </div>
     )
 }
