a83822b13b3be72100e092326d73d7db4dec46d6
Paulo Jorge Campos Cardoso <pjcardoso25@gmail.com>
2024-08-19 16:48:20 -0300

    feat(back):add ao assentosRoutes o route de update assento

--- src-back/routes/assentosRoutes.js
+++ src-back/routes/assentosRoutes.js
@@ -6,5 +6,7 @@ import findAssento from '../middlewares/findAssentoMidware.js';
 //get o assento especifico pelo id dele
 assentosRouter.get('/:secaoId/:assentoId',findAssento,(req,res)=>getAssento(req,res))
 
+//update o assento
+assentosRouter.patch('/:secaoId/:assentoId',findAssento,(req,res)=>updateAssento(req,res))
 
 export default assentosRouter;
