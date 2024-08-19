854614d940066c96c5d0cfa74c4e03504edd4b1a
Paulo Jorge Campos Cardoso <pjcardoso25@gmail.com>
2024-08-19 17:04:17 -0300

    feat(back): add ao assentosRoutes a route que permite listar os assentos

--- src-back/routes/assentosRoutes.js
+++ src-back/routes/assentosRoutes.js
@@ -1,7 +1,10 @@
 import express from 'express';
 const assentosRouter = express.Router();
-import { getAssento, updateAssento } from '../controllers/assentosController.js';
+import { getAssento, updateAssento,listAssentos } from '../controllers/assentosController.js';
 import findAssento from '../middlewares/findAssentoMidware.js';
+
+//listar assentos
+assentosRouter.get('/:secaoId',(req,res)=>listAssentos(req,res))
 
 //get o assento especifico pelo id dele
 assentosRouter.get('/:secaoId/:assentoId',findAssento,(req,res)=>getAssento(req,res))
