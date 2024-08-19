c759cee88585a85c8cddf1abbe31e8e53c613c38
Vitor Lemos <vt.lemos1701@gmail.com>
2024-08-19 00:32:38 -0300

    feat: função para achar uma porta para hospedar o servidor

--- src-back/server.js
+++ src-back/server.js
@@ -1,12 +1,35 @@
 import express from 'express';
 import createDataBase from '../script/createDataBase.js';
+const PORT = 3000;
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(3333, () => {
-    console.log("O servidor está rodando");
-});
+
+async function findAvailablePort(port) {
+  return new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      server.close(() => resolve(port)); // Porta disponível
+    });
+    server.on('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        resolve(findAvailablePort(port + 1)); // Tenta a próxima porta
+      } else {
+        reject(err);
+      }
+    });
+  });
+}
+
+findAvailablePort(PORT)
+  .then((availablePort) => {
+    app.listen(availablePort, () => {
+      console.log(`Servidor rodando na porta ${availablePort}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Erro ao tentar encontrar uma porta disponível:', err);
+  });
 
 createDataBase();
