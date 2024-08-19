6e271bb5ec61ed087da2486c8265bf3ff01f720b
Paulo Jorge Campos Cardoso <pjcardoso25@gmail.com>
2024-08-19 17:03:23 -0300

    feat(back): add ao assentosController função de listar todos os assentos de um seção

--- src-back/controllers/assentosController.js
+++ src-back/controllers/assentosController.js
@@ -1,5 +1,24 @@
 import { Assentos } from "../models/allModels.js";
 
+async function listAssentos(req,res) {
+    try{
+        const { secaoId:secaoId } = req.params
+        const assentos = await Assentos.findAll({
+            where:{
+                secaoId:secaoId
+            }
+        })
+        if(!assentos){
+            return res.status(404).json({error:"assentos não encontrados"})
+        }
+        return res.status(200).json(assentos)
+
+    }catch(error){
+        console.error("houve um erro", error);
+        return res.status(500).json({ error: "erro ao tentar fazer a requisicao" });
+    }
+}
+
 async function getAssento(req, res) {
     try {
         const assento = req.assento
@@ -29,5 +48,6 @@ async function updateAssento(req, res) {
 
 export {
     getAssento,
-    updateAssento
+    updateAssento,
+    listAssentos
 };
