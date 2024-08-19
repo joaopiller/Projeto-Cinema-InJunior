a95d5001cc0e03dcf2565d8ed2c800d090a9da0d
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-08-19 12:29:02 -0300

    feat(front): criei a navegação

--- src/components/LoginBox/index.jsx
+++ src/components/LoginBox/index.jsx
@@ -3,20 +3,26 @@ import styles from './styles.module.css'
 
 export default function LoginBox() {
     const navigate = useNavigate()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        navigate('/')
+    }
+
     return (
         <div className={styles.loginBox}>
             <div className={styles.loginHeader}>
                 <h2>Login</h2>
                 <h3>Faça login e garanta seu lugar na diversão</h3>
             </div>
-            <div className={styles.loginInputs}>
-                <input type="text" placeholder='Usuário ou E-mail'/>
+            <form onSubmit={handleSubmit} className={styles.loginInputs}>
+                <input type="text" placeholder='Usuário ou E-mail' required/>
                 <div className={styles.password}>
-                    <input type="password" placeholder='Senha'/>
+                    <input type="password" placeholder='Senha' required/>
                     <a href="#">Esqueci minha senha</a>
                 </div>
                 <button className={styles.entrarButton}>Entrar</button>
-            </div>
+            </form>
             <button onClick={() => navigate('/registrar')} className={styles.cadastrarButton}>Cadastre-se</button>
         </div>
     )
--- src/pages/Registrar/index.jsx
+++ src/pages/Registrar/index.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from 'react'
 import styles from './styles.module.css'
+import AlertModal from '../../components/AlertModal'
+import { useNavigate } from 'react-router-dom'
 
 export default function Registrar() {
+    const [modal, setModal] = useState(false)
+    const navigate = useNavigate()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        toggleModal()
+    }
+
+    function toggleModal() {
+        if (modal === false) {
+            setModal(!modal)
+        } else {
+            navigate('/login')
+        }
+    } 
+
+    useEffect(() => {
+        if (modal) {
+            document.body.style.overflowY = 'hidden'
+        } else {
+            document.body.style.overflowY = 'auto'
+        }
+        return () => {
+            document.body.style.overflowY = 'auto'
+        }
+    }, [modal])
+
     return (
         <div className={styles.registerContainer}>
             <div className={styles.registerHeader}>
@@ -9,7 +39,7 @@ export default function Registrar() {
                 <p>Seja bem-vindo à nossa comunidade apaixonada pelo mundo do cinema. Ao fazer parte do nosso espaço digital, você está prestes a mergulhar em uma experiência cinematográfica única, onde a magia das telonas ganha vida com um toque moderno.<br/>
                 Nosso formulário de cadastro é o primeiro passo para embarcar nessa jornada emocionante. Ao preenchê-lo, você se tornará um membro da nossa comunidade, onde amantes do cinema se reúnem para compartilhar o entusiasmo, as emoções e as histórias que permeiam cada cena.</p>
             </div>
-            <div className={styles.registerBox}>
+            <form onSubmit={handleSubmit} className={styles.registerBox}>
                 <h2>Registre-se</h2>
                 <input type="text" placeholder='Nome' required></input>
                 <input type="text" placeholder='Sobrenome' required></input>
@@ -18,9 +48,10 @@ export default function Registrar() {
                 <input type="text" placeholder='Nome de Usuário' required></input>
                 <input type="email" placeholder='E-mail' required></input>
                 <input type="password" placeholder='Senha' required></input>
-                <input type="text" placeholder='Confirmar senha' required></input>
-                <button>REGISTRAR</button>
-            </div>
+                <input type="password" placeholder='Confirmar senha' required></input>
+                <button type='submit'>REGISTRAR</button>
+            </form>
+            {modal && <AlertModal toggleModal={toggleModal} title='Cadastro Criado!' subtitle='Bem-Vindo à Nossa Comunidade Cinematográfica!' text='Obrigado por se juntar a nós na nossa comunidade cinematográfica. Sua jornada para uma experiência cinematográfica única começa agora. Você será redirecionado em instantes para página de login em instantes.' />}
         </div>
     )
 }
