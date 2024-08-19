489b0b05949f9cfbda2e0260195b6e78c07d6cf5
flavioleonard <115761732+flavioleonard@users.noreply.github.com>
2024-08-19 12:59:54 -0300

    CommitBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+//pages 
+import Home from './pages/Home/index'
+import FaleConosco from './pages/Fale-Conosco'
+import Filmes from './pages/Filmes'
+import Login from './pages/Login'
+import Registrar from './pages/Registrar'
 
 export default function App() {
-  return <RouterProvider router={router}/>
+  return (  
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/faleconosco' element={<FaleConosco/>}></Route>
+        <Route path='/filmes' element={<Filmes/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/registrar' element={<Registrar/>}></Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
 }
