c508e09f9a3bdd8b346b4275ddab05f7a8ae02e8
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-08-19 11:17:35 -0300

    feat(front): criei e estilizei a pag de registro

--- src/pages/Registrar/index.jsx
+++ src/pages/Registrar/index.jsx
@@ -2,6 +2,25 @@ import styles from './styles.module.css'
 
 export default function Registrar() {
     return (
-        <h1>oi</h1>
+        <div className={styles.registerContainer}>
+            <div className={styles.registerHeader}>
+                <h1>Junte-se à Comunidade Cinematográfica!<br/>
+                Cadastre-se Aqui!</h1>
+                <p>Seja bem-vindo à nossa comunidade apaixonada pelo mundo do cinema. Ao fazer parte do nosso espaço digital, você está prestes a mergulhar em uma experiência cinematográfica única, onde a magia das telonas ganha vida com um toque moderno.<br/>
+                Nosso formulário de cadastro é o primeiro passo para embarcar nessa jornada emocionante. Ao preenchê-lo, você se tornará um membro da nossa comunidade, onde amantes do cinema se reúnem para compartilhar o entusiasmo, as emoções e as histórias que permeiam cada cena.</p>
+            </div>
+            <div className={styles.registerBox}>
+                <h2>Registre-se</h2>
+                <input type="text" placeholder='Nome' required></input>
+                <input type="text" placeholder='Sobrenome' required></input>
+                <input type="text" placeholder='CPF' required></input>
+                <input type="text" placeholder='Data de nascimento' required></input>
+                <input type="text" placeholder='Nome de Usuário' required></input>
+                <input type="email" placeholder='E-mail' required></input>
+                <input type="password" placeholder='Senha' required></input>
+                <input type="text" placeholder='Confirmar senha' required></input>
+                <button>REGISTRAR</button>
+            </div>
+        </div>
     )
 }
