5ca594064e238f4af84f4c484fbc735967a5b344
João Vicente Piller Menezes <joaovicentepm14@gmail.com>
2024-08-20 11:51:19 -0300

    feat(feat): comentando css pags

--- src/pages/Filmes/index.jsx
+++ src/pages/Filmes/index.jsx
@@ -11,7 +11,6 @@ export default function Filmes() {
     const totalMovies = 20; 
     const [offset, setOffset] = useState(0);
 
-    // Simulando um array de filmes
     const allMovies = Array(totalMovies).fill({
         movieCoverImg: 'https://static.wixstatic.com/media/737e63_a9c4fd0ebd9648dfbeecdafd328ea33c~mv2.jpg/v1/fill/w_700,h_1000,al_c,q_85,usm_0.66_1.00_0.01/737e63_a9c4fd0ebd9648dfbeecdafd328ea33c~mv2.jpg',
         movieName: 'Elementos',
