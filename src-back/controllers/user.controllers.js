51bdb32ba938ba2f0fccd0bb706c085bc87a78d2
Paulo Jorge Campos Cardoso <pjcardoso25@gmail.com>
2024-08-20 13:18:58 -0300

    feat(back): add UserControler funçao get login

--- src-back/controllers/user.controllers.js
+++ src-back/controllers/user.controllers.js
@@ -1,4 +1,5 @@
-import { Users } from "../models/allModels";
+import { Users } from "../models/allModels.js";
+import { Op } from 'sequelize'
 
 // Criar um filme 
 
@@ -13,7 +14,7 @@ async function createUser(req, res) {
             username,
             email,
             senha
-        } = request.body;
+        } = req.body;
 
 
         const user = await Users.create({
@@ -26,11 +27,56 @@ async function createUser(req, res) {
             senha
         });
 
-        return request.status(201).json(user);
+        return res.status(201).json(user);
     }
     catch (error) {
-        return response.status(400).json({error: "Erro na criação do usuário."});
+        return res.status(400).json({error: "Erro na criação do usuário."});
     }
 }
 
-export default createUser
+async function loginUser(req, res) {
+    try {
+        const { username, email, senha } = req.body; 
+
+        if (!senha || (!username && !email)) {
+            console.warn("Campos faltando: Nome de usuário ou e-mail e senha são necessários.");
+            return res.status(400).json({ error: 'Nome de usuário ou e-mail e senha são necessários.' });
+        }
+
+        const user = await Users.findOne({
+            where: {
+                [Op.or]: [{ username }, { email }]
+            }
+        });
+        
+        if (!user) {
+            console.warn("Usuário não encontrado:", { username, email });
+            return res.status(404).json({ error: 'Usuário não encontrado.' });
+        }
+
+
+        if(senha != user.senha){
+            return res.status(401).json({ error: 'Senha incorreta.' });
+        }
+
+        console.log("Login bem-sucedido para o usuário:", user.username);
+
+        res.status(200).json({
+            id: user.id,
+            username: user.username,
+            nome: user.nome,
+            sobrenome: user.sobrenome,
+        });
+    } catch (error) {
+        console.error("Erro no processo de login:", error);  // Log detalhado do erro
+        res.status(500).json({ error: 'Erro ao processar a requisição.' });
+    }
+};
+
+
+
+
+export{
+    createUser,
+    loginUser,
+}
