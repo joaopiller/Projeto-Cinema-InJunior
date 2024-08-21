aec4b31ac1792579098aabe6b5cb22254e2e8ee7
Paulo Jorge Campos Cardoso <pjcardoso25@gmail.com>
2024-08-21 00:35:35 -0300

    feat(back):add filmes Router a rota de filmes

--- src-back/routes/filmes.routes.js
+++ src-back/routes/filmes.routes.js
@@ -8,7 +8,7 @@ import { checkFilmExists } from '../middlewares/checkFilmExistsMiddleware.js';
 import findFilm from "../middlewares/findFilmByIdMiddleware.js"
 
 // Inclusão dos Controllers
-import {CreateFilm, findFilmByClassification, findFilmByGenre, deleteFilm, getAllFilms} from "../controllers/filmController.js";
+import {CreateFilm, findFilmByClassification, findFilmByGenre, deleteFilm, getAllFilms, updateFilmAll, getFilm} from "../controllers/filmController.js";
 
 // Criar filme
 filmRoutes.post('/', checkFilmExists, (req, res)=> CreateFilm(req, res));
@@ -24,5 +24,11 @@ filmRoutes.delete('/:id', findFilm, (req, res)=> deleteFilm(req, res))
 
 // Listar todos os filmes
 filmRoutes.get('/', getAllFilms);
+
+// get filme especifico
+filmRoutes.get('/:id',findFilm,(req,res)=> getFilm(req,res))
+
+// update filme por completo
+filmRoutes.put('/:id',findFilm,(req,res)=>updateFilmAll(req,res))
 
 export default filmRoutes;
