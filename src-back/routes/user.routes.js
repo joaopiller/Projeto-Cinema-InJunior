3b576df194936791739737e5ed5142ab51cb770b
Paulo Jorge Campos Cardoso <pjcardoso25@gmail.com>
2024-08-21 19:48:32 -0300

    feat(back): incluindo o novo midleware na rota de criar usuario

--- src-back/routes/user.routes.js
+++ src-back/routes/user.routes.js
@@ -3,13 +3,13 @@ import express from 'express';
 const userRoutes = express.Router();
 
 // Inclusão dos middlewares
-
+import checkUserExist from '../middlewares/checkUserExistsMiddleware.js';
 
 // Inclusão dos Controllers
 import { createUser, loginUser } from "../controllers/user.controllers.js"
 
 // Criar filme
-userRoutes.post('/', createUser);
+userRoutes.post('/',checkUserExist,(req,res)=>createUser(req,res));
 
 //login User
 userRoutes.get('/login', loginUser);
